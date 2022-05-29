cdbcb1c393203fb21622f4829c1a8689927dd145
davine94 <davine94>
2022-05-29 16:21:50 +0300

    Added the first model for the location

--- gallery/settings.py
+++ gallery/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,6 +88,8 @@ DATABASES = {
         
     }
 }
+
+# adding config
 
 
 # Password validationy
