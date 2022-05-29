3f36794b947916254d6d21e7dce530fc3b4e4eb2
davine94 <davine94>
2022-05-29 20:45:20 +0300

    Added working functions for the view page

--- media/views.py
+++ media/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Image
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse, Http404
 
 # Create your views here.
+def gallery(request):
+    categories = Image.objects.distinct().values_list('category__name', flat=True)
+    locations = Image.objects.distinct().values_list('location__name', flat=True)
+    try:
+        images = Image.objects.all()
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request, 'gallery.html', {'images': images, 'locations':locations, 'categories': categories})
+def search_images(request):
+    if 'image' in request.GET and request.GET['image']:
+        search_term = request.GET('image')
+        searched_images = Image.search_by_name(search_term)
+        message = f'{search_term}'
+        return render(request, 'search.html', {'message': message, 'images': searched_images})
+    else:
+        message = "You have not searched for any term"
+        return render(request, 'search.html', {'message': message})
 def home(request):
     
     return render(request, 'media/home.html')
